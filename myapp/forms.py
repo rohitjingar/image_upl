bedb76c16ed7344ff473d6140db2327957993209
rohitjingar <rohitjingar264@gmail.com>
2022-02-02 11:25:40 +0530

    first commit

---
+++ myapp/forms.py
@@ -0,0 +1,9 @@
+from dataclasses import field
+from django import forms
+from .models import Image
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields ='__all__'
+        labels ={'photo':''}
